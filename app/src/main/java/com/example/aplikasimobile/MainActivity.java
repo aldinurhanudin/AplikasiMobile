39198e0c5b4d41499aaffda13ba1a32756ad27af
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-08-04 01:30:18 +0700

    first

--- app/src/main/java/com/example/aplikasimobile/MainActivity.java
+++ app/src/main/java/com/example/aplikasimobile/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.aplikasimobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
